21fbf7e011345e277169629977b39051e578feaa
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-12 19:09:48 -0400

    pass props data into html

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+const PhotoListItem = (props) => {
+  const {id, location, urls, user} = props.photoListItem;
 
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <article className="photo-list__item">
+    <PhotoFavButton />
+    <img src={urls.regular} alt="Image" className="photo-list__image"/>
+    <div className="photo-list__user-details">
+    <img src={user.profile} alt="Profile" className="photo-list__user-profile"/>
+    <div>
+    <p className="photo-list__user-info">{user.name}</p>
+    <p className="photo-list__user-info photo-list__user-location">{location.city}, {location.country} </p>
+    </div>
+    </div> 
+  </article>
+  )
+
 };
 
 export default PhotoListItem;
