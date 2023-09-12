df895e6e1d71e9eab7f35cb98b1278b8de5889bf
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-12 19:07:05 -0400

    add helper function to render PhotoListItems multiple times. Comment out logic about PhotoList Item and use PhotoList as a container for all PhotoList Items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
+//import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
-const App = () => {
+//helpfer function for rendering <PhotolistItem /> multiple times
+const photoListItemsMutiplyBy = (times) => {
+  return Array.from({ length: times }, (_, index) => {
+    return {
+      ...sampleDataForPhotoListItem,
+      //increment id by one for each rendering which will be used as value for key 
+      id: `${index + 1}`,
+    };
+  });
+};
+
+const App = (props) => {
+
+  // const multiplePhotoListItems = photoListItemsMutiplyBy(3).map((item)=>{
+  //  return <PhotoListItem sampleDataForPhotoListItem={item} key={item.id}/>
+  // })
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      {/* {multiplePhotoListItems} */}
+      <PhotoList />
     </div>
   );
 };
