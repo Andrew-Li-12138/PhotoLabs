13d7f48da17049b79948a997c6ce551ce2f1a5cd
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 16:44:19 -0400

    pass managePhotoClick to PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,19 +3,20 @@ import TopNavigation from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss'
 
-
 const HomeRoute = (props) => {
-  const {photos, topics} = props;
+  const {photos, topics, managePhotoClick} = props;
   const [count, setCount] = useState(0);
+  //state to pass down to FavBadge and PhotoLisrItem for fav count num
   const countClick = (increment) => {
     setCount(count + increment);
   }
   const isFavPhotoExist = count > 0 ? true : false;
 
+
   return (
     <div className="home-route">
     <TopNavigation topics={topics} count={count} isFavPhotoExist={isFavPhotoExist}/>
-    <PhotoList photos={photos} countClick={countClick}/>
+    <PhotoList photos={photos} countClick={countClick} managePhotoClick={managePhotoClick}/>
     </div>
   )
 }
