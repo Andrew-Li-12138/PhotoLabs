3ce7f638799c1bfca47ea064070048612052891c
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-12 21:32:22 -0400

    delete unused code and use HomeRoute to show homepage components. pass mock data to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
-
-//import PhotoListItem from './components/PhotoListItem';
-import PhotoList from 'components/PhotoList';
-//import TopicListItem from 'components/TopicListItem';
-import TopicList from 'components/TopicList';
+import HomeRoute from 'components/HomeRoute'
+import photos from 'mocks/photos';
+import topics from 'mocks/topics'
 import './App.scss';
-
-//helpfer function for rendering <PhotolistItem /> multiple times
-const photoListItemsMutiplyBy = (times) => {
-  return Array.from({ length: times }, (_, index) => {
-    return {
-      ...sampleDataForPhotoListItem,
-      //increment id by one for each rendering which will be used as value for key 
-      id: `${index + 1}`,
-    };
-  });
-};
 
 const App = (props) => {
 
-  // const multiplePhotoListItems = photoListItemsMutiplyBy(3).map((item)=>{
-  //  return <PhotoListItem sampleDataForPhotoListItem={item} key={item.id}/>
-  // })
-
   return (
     <div className="App">
-      {/* {multiplePhotoListItems} */}
-      <TopicList />
-      <PhotoList />
+     <HomeRoute photos={photos} topics={topics}/>
     </div>
   );
 };
