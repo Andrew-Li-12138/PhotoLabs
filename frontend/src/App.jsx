82c10400b05daee18e4b93f2ff4961f400305231
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 16:42:38 -0400

    add state and function to toggle clicked state and pass state components & use for conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HomeRoute from 'routes/HomeRoute'
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics'
+
 import './App.scss';
 
 const App = (props) => {
+  //state for showing <PhotoDetailsModal /> component
+  const [clicked, setClicked] = useState(false)
+  const managePhotoClick = () => {
+    setClicked(!clicked)
+  }
 
   return (
     <div className="App">
-     <HomeRoute photos={photos} topics={topics}/>
+     <HomeRoute photos={photos} topics={topics} managePhotoClick={managePhotoClick}/>
+     {clicked && <PhotoDetailsModal manageCloseClick={managePhotoClick}/>}
     </div>
   );
 };
