bce608c377f646021fb0fdb0a825559e747eddce
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 15:33:03 -0400

    move HomeRoute from component folder to routes folder

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+import TopNavigation from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
+import '../styles/HomeRoute.scss'
 
-import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+  const {photos, topics} = props;
+  const [count, setCount] = useState(0);
+  const countClick = (increment) => {
+    setCount(count + increment);
+  }
+  const isFavPhotoExist = count > 0 ? true : false;
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+    <TopNavigation topics={topics} count={count} isFavPhotoExist={isFavPhotoExist}/>
+    <PhotoList photos={photos} countClick={countClick}/>
     </div>
-  );
-};
+  )
+}
 
 export default HomeRoute;
