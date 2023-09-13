20cd0e1bdcd985ca62b7bcc4971595c570770e7b
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 15:37:42 -0400

    use the value of count and pass isFavPhotoExist down to FavIcon

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,10 +3,13 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = (props) => {
+  const { isFavPhotoExist, count } = props
+  
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+      <FavIcon displayAlert={isFavPhotoExist} selected={isFavPhotoExist}/>
+      <span>{count}</span>
     </div>
   ) 
 };
