6ef87c4064687cc896c729c4a2a412bd79aa3806
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 15:34:19 -0400

    delete sample data and use mock data passed down as props

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,25 +3,8 @@ import TopicListItem from "./TopicListItem";
 
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-]; 
-
-const TopicList = () => {
+const TopicList = (props) => {
+  const sampleDataForTopicList = props.topics;
   return (
     <div className="top-nav-bar__topic-list">
     {sampleDataForTopicList.map((topicListItem) => {
