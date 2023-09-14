be3833bcc7b8eee80aed6a31d6a3a9389e255109
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-14 15:51:07 -0400

    remove displayAlert state as it is not needed per UI requirement

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,12 +4,10 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
   const [selected, setSelect] = useState(false);
-  const [displayAlert, setDisplayAlert] = useState(false);
   const countClick = props.countClick;
 
  const handleFavButtonClick = () => { 
     setSelect(!selected);
-    setDisplayAlert(!displayAlert)
     if (!selected) {
       // If the icon was not selected, increment the count
       countClick(1);
@@ -22,7 +20,7 @@ function PhotoFavButton(props) {
   return (
     <div className="photo-list__fav-icon"  onClick={handleFavButtonClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon displayAlert={displayAlert} selected={selected}/>
+        <FavIcon selected={selected}/>
       </div>
     </div> 
   );
