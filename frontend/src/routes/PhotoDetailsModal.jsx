935f8ff3c7cb8e98fcbf154890d4afb65a51be2a
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 22:05:53 -0400

    take and store photoItemDetails data

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,10 +4,14 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
-  const { manageCloseClick } = props
+  const { managePhotoClick, photoItemDetails} = props;
+  const {id, location, urls, user} = photoItemDetails;
+  console.log(id, location, urls, user)
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={ manageCloseClick }>
+
+      <button className="photo-details-modal__close-button" onClick={ managePhotoClick }>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
