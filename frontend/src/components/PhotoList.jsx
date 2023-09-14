b84cc9d32031b185a08bcba66f19c09e5b96b4e4
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-13 23:28:57 -0400

    map entire photo data so similar data can be included

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,12 +4,12 @@ import "../styles/PhotoList.scss";
 
 
 const PhotoList = (props) => {
-  const sampleDataForPhotoList = props.photos
+  const sampleDataForPhotoList = props.photos 
   const {countClick, managePhotoClick, getPhotoItemDetails} = props
 
   return (
     <ul className="photo-list">
-    {sampleDataForPhotoList.map((photoListItem)=>{ 
+    {sampleDataForPhotoList.map((photoListItem)=>{  
      return <PhotoListItem key={photoListItem.id} photoListItem={photoListItem} countClick={countClick} managePhotoClick={managePhotoClick} 
      getPhotoItemDetails={getPhotoItemDetails}/>
     })}
@@ -17,4 +17,4 @@ const PhotoList = (props) => {
   );
 };
 
-export default PhotoList;
+export default PhotoList; 
