893f24961901657da2c8f6ca8bfbfa5e5877eb64
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-14 21:19:26 -0400

    adjust the order of how functions are called during onClick so data are rendered properly upon onClick

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,21 @@ import "../styles/PhotoListItem.scss";
 
 
 const PhotoListItem = (props) => {
+  console.log(props.photoListItem)
   const {id, location, urls, user} = props.photoListItem;
   const {countClick, managePhotoClick, getPhotoItemDetails} = props;
+
+  const handleClick = ()=> {
+    console.log(props.photoListItem)
+    getPhotoItemDetails(props.photoListItem)
+    managePhotoClick()
+    
+  }
 
   return (
     <article className="photo-list__item">
     <PhotoFavButton countClick={countClick}/>
-    <img src={urls.regular} alt="Image" className="photo-list__image" onClick={()=>{managePhotoClick();getPhotoItemDetails(props.photoListItem)}}/>
+    <img src={urls.regular} alt="Image" className="photo-list__image" onClick={handleClick}/>
     <div className="photo-list__user-details">
     <img src={user.profile} alt="Profile" className="photo-list__user-profile"/>
     <div>
@@ -23,4 +31,4 @@ const PhotoListItem = (props) => {
 
 }; 
 
-export default PhotoListItem;
+export default PhotoListItem; 
