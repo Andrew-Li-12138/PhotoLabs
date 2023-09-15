f692dfe4394fdf5330ff3da915b1d3cd950a222c
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-15 14:58:57 -0400

    modify prop select to reference to selectedPhotos state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,24 +6,26 @@ import topics from 'mocks/topics'
 import { useApplicationData } from 'hooks/useApplicationData';
 import './App.scss';
 
-const App = (props) => {
+const App = (props) => { 
 
 
   const {
     state,
     managePhotoClick,
+    selectedOrNot,
     getPhotoItemDetails,
     countClick,
     } = useApplicationData()
 
   const isFavPhotoExist = state.count > 0 ? true : false;
  
-
-  
   return (
     <div className="App">
-     <HomeRoute photos={photos} topics={topics} count={state.count} isFavPhotoExist={isFavPhotoExist} countClick={countClick} managePhotoClick={managePhotoClick} getPhotoItemDetails={getPhotoItemDetails}/>
-     {state.clicked && <PhotoDetailsModal managePhotoClick={managePhotoClick}  photoItemDetails={state.photoItemDetails} countClick={countClick}/>}
+     <HomeRoute photos={photos} topics={topics} count={state.count} selected={state.selectedPhotos} selectedOrNot={selectedOrNot}
+     isFavPhotoExist={isFavPhotoExist} countClick={countClick} managePhotoClick={managePhotoClick} getPhotoItemDetails={getPhotoItemDetails}/>
+     {state.clicked && <PhotoDetailsModal selected={state.selectedPhotos} selectedOrNot={selectedOrNot} managePhotoClick={managePhotoClick} 
+     photoItemDetails={state.photoItemDetails} countClick={countClick}/>}
+      {console.log(state.selectedPhotos)}
     </div>
   );
 };
