314993b7cf71f38fa7f72e9c3f6eb144bc6c807d
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-15 15:00:10 -0400

    pass selectedOrNot function to use in handleFavButtonClick to check individual photo's selected state

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,11 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
-  const [selected, setSelect] = useState(false);
-  const countClick = props.countClick;
 
- const handleFavButtonClick = () => { 
-    setSelect(!selected);
+  const {countClick, selected, selectedOrNot, id} = props;
+
+  const handleFavButtonClick = () => { 
+    selectedOrNot(id)
     if (!selected) {
       // If the icon was not selected, increment the count
       countClick(1);
@@ -16,9 +16,8 @@ function PhotoFavButton(props) {
       countClick(-1);
     }
   }
-
   return (
-    <div className="photo-list__fav-icon"  onClick={handleFavButtonClick}>
+    <div className="photo-list__fav-icon" onClick={handleFavButtonClick}>
       <div className="photo-list__fav-icon-svg">
         <FavIcon selected={selected}/>
       </div>
