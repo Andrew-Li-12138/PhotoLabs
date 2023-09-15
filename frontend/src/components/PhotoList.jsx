6c73744606f609f975c3143452024c010a97da17
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-15 15:01:52 -0400

    modify selected so it takes id to check true/false

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -5,12 +5,12 @@ import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
   const sampleDataForPhotoList = props.photos 
-  const {countClick, managePhotoClick, getPhotoItemDetails} = props
+  const {countClick, managePhotoClick, getPhotoItemDetails, selected, selectedOrNot} = props
 
   return (
     <ul className="photo-list">
     {sampleDataForPhotoList.map((photoListItem)=>{  
-     return <PhotoListItem key={photoListItem.id} photoListItem={photoListItem} countClick={countClick} managePhotoClick={managePhotoClick} 
+     return <PhotoListItem key={photoListItem.id} selected={selected[photoListItem.id]||false} selectedOrNot={() => selectedOrNot(photoListItem.id)} photoListItem={photoListItem} countClick={countClick} managePhotoClick={managePhotoClick} 
      getPhotoItemDetails={getPhotoItemDetails}/>
     })}
     </ul>
