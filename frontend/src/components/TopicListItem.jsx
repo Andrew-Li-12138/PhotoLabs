e8eefec29f59798c56321772fed488c9cc7a1011
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-16 01:33:36 -0400

    use getTopicId to get id of topicListItem and pass back to useApplicationData to update state.topicId

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import "../styles/TopicListItem.scss";
+import HomeRoute from "routes/HomeRoute";
 
 const TopicListItem = (props) => {
-  const {id, slug, title} = props.topicListItem
+  const { id, slug, title } = props.topicListItem
+  const {getTopicId} = props
+ 
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={()=>getTopicId(id)}>
       <span>{title}</span>
     </div>
   );
