ba898d99074317ac768f41f1210e0cee42a7919e
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-16 01:34:06 -0400

    pass getTopicId down

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,11 +4,11 @@ import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss'
 
 const HomeRoute = (props) => {
-  const {photos, topics, selected, selectedOrNot, managePhotoClick, getPhotoItemDetails, count, countClick, isFavPhotoExist} = props;
+  const {photos, topics, getTopicId, selected, selectedOrNot, managePhotoClick, getPhotoItemDetails, count, countClick, isFavPhotoExist} = props;
 
   return (
     <div className="home-route">
-    <TopNavigation topics={topics} count={count} isFavPhotoExist={isFavPhotoExist}/>
+    <TopNavigation topics={topics} getTopicId={getTopicId} count={count} isFavPhotoExist={isFavPhotoExist}/>
     <PhotoList photos={photos} selected={selected} selectedOrNot={selectedOrNot} countClick={countClick} managePhotoClick={managePhotoClick} getPhotoItemDetails={getPhotoItemDetails}/>
     </div>
   )
