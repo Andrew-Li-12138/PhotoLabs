56a98ffc3a7801aa0f63c7510fdfabc27a78dfb4
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-19 18:51:52 -0400

    add span and call displaySelectedPhoto function onClick

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,11 +4,12 @@ import TopicList from 'components/TopicList';
 import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = (props) => {
-  const {topics, getTopicId, count, isFavPhotoExist} = props
+  const {topics, getTopicId, count, isFavPhotoExist, displaySeletcedPhotos} = props
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} getTopicId={getTopicId}/>
+      <TopicList topics={topics} getTopicId={getTopicId} displaySeletcedPhotos={displaySeletcedPhotos}/>
+      <span onClick={()=>{displaySeletcedPhotos()}} className="top-nav-bar__fav">Favorites(in Category)</span>
       <FavBadge count={count} isFavPhotoExist={isFavPhotoExist}/>
     </div>
   )
