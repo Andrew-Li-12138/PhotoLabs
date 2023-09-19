82e87c1f8a13bac05630eac2e4447cf3c7fd744d
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-19 18:50:21 -0400

    pass displaySelectedPhoto function to TopNavigation

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,11 +4,12 @@ import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss'
 
 const HomeRoute = (props) => {
-  const {photos, topics, getTopicId, selected, selectedOrNot, managePhotoClick, getPhotoItemDetails, count, countClick, isFavPhotoExist} = props;
+  const {photos, topics, getTopicId, selected, selectedOrNot, managePhotoClick, getPhotoItemDetails, 
+    count, countClick, isFavPhotoExist, displaySeletcedPhotos} = props;
 
   return (
     <div className="home-route">
-    <TopNavigation topics={topics} getTopicId={getTopicId} count={count} isFavPhotoExist={isFavPhotoExist}/>
+    <TopNavigation topics={topics} getTopicId={getTopicId} count={count} isFavPhotoExist={isFavPhotoExist} displaySeletcedPhotos={displaySeletcedPhotos}/>
     <PhotoList photos={photos} selected={selected} selectedOrNot={selectedOrNot} countClick={countClick} managePhotoClick={managePhotoClick} getPhotoItemDetails={getPhotoItemDetails}/>
     </div>
   )
