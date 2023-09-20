d1b362f108c4fcbbe1e2cb2ac3e5e8137a5d55cd
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-19 20:41:32 -0400

    pass darkMode state down

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,12 +5,13 @@ import '../styles/HomeRoute.scss'
 
 const HomeRoute = (props) => {
   const {photos, topics, getTopicId, selected, selectedOrNot, managePhotoClick, getPhotoItemDetails, 
-    count, countClick, isFavPhotoExist, displaySeletcedPhotos} = props;
-
+    count, countClick, isFavPhotoExist, displaySeletcedPhotos, darkMode} = props;
+    
   return (
     <div className="home-route">
     <TopNavigation topics={topics} getTopicId={getTopicId} count={count} isFavPhotoExist={isFavPhotoExist} displaySeletcedPhotos={displaySeletcedPhotos}/>
-    <PhotoList photos={photos} selected={selected} selectedOrNot={selectedOrNot} countClick={countClick} managePhotoClick={managePhotoClick} getPhotoItemDetails={getPhotoItemDetails}/>
+    <PhotoList photos={photos} selected={selected} selectedOrNot={selectedOrNot} countClick={countClick} managePhotoClick={managePhotoClick} 
+    getPhotoItemDetails={getPhotoItemDetails} darkMode={darkMode}/>
     </div>
   )
 }
