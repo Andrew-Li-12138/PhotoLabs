07a1a9139e994c0c25f91c48292d65737296e1f8
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-19 20:41:55 -0400

    pass darkMode state down

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -5,13 +5,13 @@ import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
   const sampleDataForPhotoList = props.photos 
-  const {countClick, managePhotoClick, getPhotoItemDetails, selected, selectedOrNot} = props
+  const {countClick, managePhotoClick, getPhotoItemDetails, selected, selectedOrNot, darkMode} = props
 
   return (
     <ul className="photo-list">
     {sampleDataForPhotoList.map((photoListItem)=>{  
      return <PhotoListItem key={photoListItem.id} selected={selected[photoListItem.id]||false} selectedOrNot={() => selectedOrNot(photoListItem.id)} photoListItem={photoListItem} countClick={countClick} managePhotoClick={managePhotoClick} 
-     getPhotoItemDetails={getPhotoItemDetails}/>
+     getPhotoItemDetails={getPhotoItemDetails} darkMode={darkMode}/>
     })}
     </ul>
   );
