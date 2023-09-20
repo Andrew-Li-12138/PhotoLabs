5b935a3fe47669b2f18824d95e2d1b669ee3c560
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-19 20:39:37 -0400

    use toggleDarkMode onClick and pass state.darkMode down

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import './App.scss';
 
 const App = (props) => { 
 
-
   const {
     state,
     managePhotoClick,
@@ -15,19 +14,21 @@ const App = (props) => {
     countClick,
     getTopicId,
     displaySeletcedPhotos,
+    toggleDarkMode,
     } = useApplicationData()
 
   const isFavPhotoExist = state.count > 0 ? true : false;
- 
+  
   return (
-    <div className="App">
+    <div className={`App ${state.darkMode ? 'App-Dark' : ''}`}>
+      <button className="App-Dark_Button" onClick={()=>{toggleDarkMode()}}>DarkMode</button>
      <HomeRoute photos={state.photoData} topics={state.topicData} getTopicId={getTopicId}
      count={state.count} selected={state.selectedPhotos} selectedOrNot={selectedOrNot}
      isFavPhotoExist={isFavPhotoExist} countClick={countClick} managePhotoClick={managePhotoClick} 
-     getPhotoItemDetails={getPhotoItemDetails} displaySeletcedPhotos={displaySeletcedPhotos}/>
+     getPhotoItemDetails={getPhotoItemDetails} displaySeletcedPhotos={displaySeletcedPhotos} darkMode={state.darkMode}/>
      
      {state.clicked && <PhotoDetailsModal selected={state.selectedPhotos} selectedOrNot={selectedOrNot} managePhotoClick={managePhotoClick} 
-     photoItemDetails={state.photoItemDetails} countClick={countClick}/>}
+     photoItemDetails={state.photoItemDetails} countClick={countClick} darkMode={state.darkMode} />}
     </div>
   );
 };
