a42528d0eddbe5adee964c0823ae9c0c327c40a8
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-09-20 17:43:35 -0400

    pass fetchAndPassPhotosData to TopNavigatio

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,11 +5,12 @@ import '../styles/HomeRoute.scss'
 
 const HomeRoute = (props) => {
   const {photos, topics, getTopicId, selected, selectedOrNot, managePhotoClick, getPhotoItemDetails, 
-    count, countClick, isFavPhotoExist, displaySeletcedPhotos, darkMode} = props;
+    count, countClick, isFavPhotoExist, displaySeletcedPhotos, darkMode, fetchAndPassPhotosData} = props;
     
   return (
     <div className="home-route">
-    <TopNavigation topics={topics} getTopicId={getTopicId} count={count} isFavPhotoExist={isFavPhotoExist} displaySeletcedPhotos={displaySeletcedPhotos}/>
+    <TopNavigation topics={topics} getTopicId={getTopicId} count={count} isFavPhotoExist={isFavPhotoExist} 
+    displaySeletcedPhotos={displaySeletcedPhotos} fetchAndPassPhotosData={fetchAndPassPhotosData}/>
     <PhotoList photos={photos} selected={selected} selectedOrNot={selectedOrNot} countClick={countClick} managePhotoClick={managePhotoClick} 
     getPhotoItemDetails={getPhotoItemDetails} darkMode={darkMode}/>
     </div>
